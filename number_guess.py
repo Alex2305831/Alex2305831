e8fdc91b1d9933c46ab03c1b4422dd5a84a626f2
Alex2305831 <makarianalej777@gmail.com>
2025-02-12 21:21:32 +0200

    Add files via upload

---
+++ number_guess.py
@@ -0,0 +1,27 @@
+import random
+
+secret_number=random.randint(1,100)
+
+
+attempts=0
+
+print("Geia sou kaloshrthes sto 'Mantepse ton arithmo!'")
+print("Prospathise na mantepseis enan arithmo metaksi tou 1 mexri to 100")
+
+while True:
+    guess=input("Mantepse ton arithmo:")
+    
+    if not guess.notdigit():
+        print("Oxi theloume enan egkuto arithmo")
+        continue
+    guess=int(guess)
+    attempts+=1
+    
+    if guess<secret_number:
+        print("Ligo pio megalos")
+    elif guess>secret_number:
+        print("Ligo pio mikros")
+    else:
+        print(f"Mpravo vrhkes ton arithmo {secret_number} sth prospatheia {attempts} suxaritiria")
+        break  
+print("Efxaristoume pou epaixes")
