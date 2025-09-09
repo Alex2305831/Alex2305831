3e97c59c6a2a65dcddf1c1174e2e63023590cde4
Alex2305831 <makarianalej777@gmail.com>
2025-09-09 13:58:16 +0300

    Add files via upload

---
+++ random.js
@@ -0,0 +1,12 @@
+function add(){
+   window.alert(parseInt(document.maths.fnumber.value)+parseInt(document.maths.snumber.value));
+}
+function sub(){
+    window.alert(parseInt(document.maths.fnumber.value)-parseInt(document.maths.snumber.value));
+}
+function mul(){
+    window.alert(parseInt(document.maths.fnumber.value)*parseInt(document.maths.snumber.value));
+}
+function div(){
+    window.alert(parseInt(document.maths.fnumber.value)/parseInt(document.maths.snumber.value));
+}
