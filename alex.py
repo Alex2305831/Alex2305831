8f1517ec945b054bfde434883a392855a0e25f23
Alex2305831 <makarianalej777@gmail.com>
2025-10-13 19:19:51 +0300

    Add files via upload

--- alex.py
+++ alex.py
@@ -1,63 +1,35 @@
-import random
-from words import words_list
+print("Vlepeis eno perpatas sto dasos duo monopatia pou mporeis na pas")
+print("1: Odhgei pros ena vouno")
+print("2: Odhgei pros ena potami")
 
-def get_word():
-    word=random.choice(words_list)
-    return word.upper()
-
-def play(word):
-    word_length="_" * len(word)
-    guessed=False
-    guessed_letters=[]
-    guessed_words=[]
-    tries=6
-    print("Lets play Hangman!")
-    print(word_length)
-    while not guessed and tries>0:
-        guess=input("Please guess a letter or word:").upper()
-        if len(guess)==1 and guess.isalpha():
-            if guess in guessed_letters:
-                print("You already guessed this letter",guess)
-                print(word_length)
-            elif guess not in word:
-                print(guess,"is not in the word")
-                tries-=1
-                print(word_length)
-                guessed_letters.append(guess) 
-            else:
-                print("Good job",guess,"is in the word")
-                guessed_letters.append(guess)
-                word_as_list=list(word_length)
-                indices=[i for i,letter in enumerate(word) if letter==guess]
-                for index in indices:
-                    word_as_list[index]=guess
-                word_length="".join(word_as_list)
-                if not "_" in word_length:
-                    guessed=True
-                print(word_length)
-        elif len(guess)==len(word) and guess.isalpha():
-            if guess in guessed_words:
-                print("You already guess the word",guess)
-            elif guess!=word:
-                print(guess,"is not the word")
-                tries-=1
-                guessed_words.append(guess)
-            else:
-                guessed=True
-                word_length=word
-        else:
-            print("Not a valid guess")
-            break
-    if guessed:
-        print("Congrats, you guessed the word you win!")
-    else:
-        print("Sorry you out of tries the word was",word,"maybe next time")
+choice=input("Ti dialegeis:")
+if choice=="1":
+    print("Pernas pros to vouno kai tha prepei na skarfaloseis pshla oste na to diaperaseis\n")
+elif choice=="2":
+    print("Pernas pros to potami kai tha prepei na peraseis apenanti apo auto oste na sunexiseis\n")
+else:
+    print("Tha prepei na pas anagkastika se ena apo ta duo monopatia allios tha pethaneis ekei")
+while choice=="1":
+    print("Exeis ftasei sto pshlo vouno ti antikeimeno tha epilekseis gia na to diaperases apo pano tou\n")
+    choice2=input("1:Eksoplismo oreivaseias me skoini\n2:Ena autokinhti ikano gia na anevei epano pshla:")
+    if choice2=="1":
+        print("Molis parelaves oloklhro eksoplismo oreivasias me makrosteno skoini ksekina thn poreia sou")
+        break
+    elif choice2=="2":
+        print("Molis parelaves ena ikano autokinhto oste na diaperasei olo to vouno me tis rodes pou antexoun arketa tis anhfores")
+        break 
+while choice=="2":
+    print("Exeis ftasei sto potami kai prepei kapos na peraseis apenanti\n")
+    choice3=input("1:Mia ksilinh varka oste na ftaseis apenanti\n2:Na pas pros thn deksia meria tou potamou pou sou exei akoma duo epiloges gia na kopseis dromo:")
+    if choice3=="1":
+        print("Oraia apokthses mia ksilinh varka gia na peraseis sthn allh meria")
+        break
+    elif choice3=="2":
+        choice4=input("Exeis akoma duo dromous (den paizei rolo pou tha pas):\n 1:deksia\n2:aristera:")
+    if choice4=="1":
+        print("Dialekses deksia exeis 2 ores dromo akomh")
+        break
+    elif choice4=="2":
+        print("Dialekses aristera exeis 1.5 ora dromo akomh")
+        break
         
-def main():
-    word=get_word()
-    play(word)
-    while input("Play Again? (Y/N)").upper()=="Y":
-        word=get_word()
-        play(word)
-        
-main()             
