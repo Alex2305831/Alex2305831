49ba24349ec1852d637885f8fd4961c5d085016a
Alex2305831 <makarianalej777@gmail.com>
2025-10-15 13:31:21 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,26 +1,42 @@
 import java.util.Scanner;
 public class Main{
+    static void myMethod(String txt){
+        System.out.println(txt.replaceAll("my","your"));
+    }
     public static void main(String[] args) {
-        try (Scanner scan = new Scanner(System.in)) {
-            String txt="Right now im practising in Java and i really like it";
-            String txt2="Right now im practising in Python and i really like it";
-            byte[] result=txt.getBytes();
-            System.out.println(result[0]);
-            char result2=txt.charAt(27);
-            System.out.println(result2);
-            do { 
-                System.out.println(txt.replaceAll("Java", "Python"));
+        myMethod("This is my text");
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Please type your text again to solve something:");
+        String txt=scan.nextLine();
+        System.out.println(txt);
+        System.out.print("Type a random number to see his multiplication operation until 10:");
+        int x=scan.nextInt();
+        for (int i=1;i<=10;i++){
+            System.out.println(x+" * "+i+" = "+x*i);
+        }
+        int y=(int)(Math.random()*11);
+        System.out.println(y);
+        switch (y){
+            case 0:
+               System.out.print("You lost you succeded 0");
+               break;
+            case 1:
+                System.out.print("You win you succeded 1");
                 break;
-            } while (txt.length()<60);
-            System.out.println(txt.compareTo(txt2));
-            System.out.println("Enter your information below");
-            System.out.print("Name:");
-            String name=scan.nextLine();
-            System.out.print("City:");
-            String city=scan.nextLine();
-            System.out.print("Age:");
-            int age=scan.nextInt();
-            System.out.println("Your name is "+name+" you are "+age+" years old and you live in "+city);
+            case 3:
+                System.out.print("You have 3 attempts because you succeded 3");
+                break;
+            case 5:
+                System.out.print("You succeded 5 so the program is success");
+                break;
+            case 7:
+                System.out.print("You are lucky because you succeded 7");
+                break;
+            case 10:
+                System.out.print("You succeded 10 so you are smart you are 10/10");
+                break;
+            default:
+                System.out.print("Number with no meaning");
         }
     }
 }
